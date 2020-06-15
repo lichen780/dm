65c218ebf99ec3b0fb8c1ffbf234629cafdbea96
lichen <39560327+lichen780@users.noreply.github.com>
2020-06-15 23:27:07 +0800

    Update wy.py

--- wy.py
+++ wy.py
@@ -4,8 +4,8 @@ def start():
 		'MUSIC_U': '158e87e371bc9b8e8bcb2d09543b425c8e786030155986b3048868b55489928833a649814e309366',
 		'__csrf': '125dd9726016ed3a44fee21e9f6e44e5',
 	}
-	res = requests.post('https://yibiwy.fun/api.php?do=sign', cookies=cookies)
-	resp = requests.get('https://yibiwy.fun/api.php?do=daka', cookies=cookies)
+	res = requests.post('http://yibiwy.fun/api.php?do=sign', cookies=cookies)
+	resp = requests.get('http://yibiwy.fun/api.php?do=daka', cookies=cookies)
 	print(res.text,resp.text)
 def main_handler(event, context):
 	return start()
