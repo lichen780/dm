87ae0aab37a567f55ee9a07cf6e8112d39a13c74
lichen <39560327+lichen780@users.noreply.github.com>
2020-06-15 23:13:36 +0800

    Add files via upload

---
+++ wy.py
@@ -0,0 +1,13 @@
+﻿import requests
+def start():
+	cookies = {
+		'MUSIC_U': '158e87e371bc9b8e8bcb2d09543b425c8e786030155986b3048868b55489928833a649814e309366',
+		'__csrf': '125dd9726016ed3a44fee21e9f6e44e5',
+	}
+	res = requests.post('https://yibiwy.fun/api.php?do=sign', cookies=cookies)
+	resp = requests.get('https://yibiwy.fun/api.php?do=daka', cookies=cookies)
+	print(res.text,resp.text)
+def main_handler(event, context):
+	return start()
+if __name__ == '__main__':
+	start()
